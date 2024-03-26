743d6daa659065c3232779f597675747c3344119
Elmer Brännström <06elbr01@minervagymnasium.se@06elbr01@minervagymnasium.se---@minervagymnasium.se---Elmer-Brannstrom>
2024-03-26 09:15:14 +0100

    Wrote new sqlx migrations file and migrated the database.

---
+++ database/migrations/20240325154739_init.sql
@@ -0,0 +1,16 @@
+-- Add migration script here
+CREATE TABLE IF NOT EXISTS project(
+    name STRING UNIQUE NOT NULL,
+    encoded_name STRING UNIQUE NOT NULL,
+    id INTEGER UNIQUE NOT NULL PRIMARY KEY AUTOINCREMENT,
+    created_at INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS columns(
+    project_id INTEGER NOT NULL,
+    name STRING NOT NULL,
+    encoded_name STRING NOT NULL,
+    created_at INTEGER NOT NULL,
+
+    FOREIGN KEY (project_id) REFERENCES project(id)
+);
--- database/src/lib.rs
+++ database/src/lib.rs
@@ -1,43 +1,8 @@
-use chrono::{DateTime, Utc};
 use sqlx::{migrate, AnyPool};
 
 #[allow(unused)]
 pub struct Database {
     pool: AnyPool,
-}
-
-#[allow(unused)]
-pub struct Dataset {
-    pool: AnyPool,
-    id: i64,
-    name: String,
-    created_at: DateTime<Utc>,
-}
-
-#[allow(unused)]
-pub struct Datastream {
-    id: i64,
-    data_type: String,
-    name: String,
-    created_at: DateTime<Utc>,
-}
-
-#[allow(dead_code)]
-#[derive(sqlx::FromRow)]
-struct RawDataset {
-    id: i64,
-    name: String,
-    created_at: i64,
-}
-
-#[allow(dead_code)]
-#[derive(sqlx::FromRow)]
-struct RawDatastream {
-    id: i64,
-    name: String,
-    dataset_id: i64,
-    data_type: String,
-    created_at: i64,
 }
 
 impl Database {
@@ -50,53 +15,4 @@ impl Database {
     }
 }
 
-impl Database {
-    pub async fn dataset(&self, name: &str) -> Dataset {
-        let result: RawDataset = sqlx::query_as(r#"SELECT * FROM Dataset WHERE name = ?"#)
-            .bind(name)
-            .fetch_one(&self.pool)
-            .await
-            .unwrap();
 
-        Dataset {
-            pool: self.pool.clone(),
-            id: result.id,
-            name: result.name,
-            created_at: DateTime::from_timestamp_millis(result.created_at).unwrap(),
-        }
-    }
-
-    pub async fn datasets(&self) -> impl Iterator<Item = Dataset> + '_ {
-        // Get all datasets as raw
-        sqlx::query_as(r#"SELECT * FROM Dataset"#)
-            .fetch_all(&self.pool)
-            .await
-            .unwrap()
-            .into_iter()
-            // Convert to Dataset
-            .map(|result: RawDataset| Dataset {
-                pool: self.pool.clone(),
-                id: result.id,
-                name: result.name,
-                created_at: DateTime::from_timestamp_millis(result.created_at).unwrap(),
-            })
-    }
-}
-
-impl Dataset {
-    pub async fn datastreams(&self) -> impl Iterator<Item = Datastream> + '_ {
-        // Get all datastreams as raw
-        sqlx::query_as(r#"SELECT * FROM Datastream WHERE dataset_id = ?"#)
-            .bind(self.id)
-            .fetch_all(&self.pool)
-            .await
-            .unwrap()
-            .into_iter()
-            .map(|result: RawDatastream| Datastream {
-                id: result.id,
-                data_type: result.data_type,
-                name: result.name,
-                created_at: DateTime::from_timestamp_millis(result.created_at).unwrap(),
-            })
-    }
-}
