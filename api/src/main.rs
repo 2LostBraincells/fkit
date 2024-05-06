2LostBraincells/fkit

commit d1b76ae9e3a52835f1ef7319bf4396c83f655b2e

Made get_data return hashmaps with optional values in case of null

database/src/project.rs
@@ -245,7 +245,7 @@ impl Project {
     }
 
     /// All datapoints from the project
-    pub async fn get_data(&self) -> Result<Vec<HashMap<String, String>>, sqlx::Error> {
+    pub async fn get_data(&self) -> Result<Vec<HashMap<String, Option<String>>>, sqlx::Error> {
         let query = format!(
             r#"
             SELECT * FROM {}

commit 7a1465f27b19390585f033a774b3403dc68b409f

Fixed test that broke after commit d1b76ae

api/src/main.rs
@@ -73,7 +73,7 @@ async fn run(config_path: Option<PathBuf>) -> Result<(), Box<dyn Error>> {
     // Create the routes
     let routes = Router::new()
         .route("/new/:project", post(create_project))
-        .route("/:project/add", post(add_datapoint))
+        .route("/:project", post(add_datapoint))
         .route("/:project/columns", post(define_columns));
 
     // Create the app
@@ -114,7 +114,10 @@ async fn add_datapoint(
     State(database): State<Database>,
 ) -> Result<String>{
     let project = match database.get_project(&project).await.map_err(|e| format!("Error: {:?}", e).into_response())? {
-        None => database.create_project(&project).await.map_err(|e| format!("Error: {:?}", e).into_response())?,
+        None => {
+            println!("Project not found, creating new: {}", project);
+            database.create_project(&project).await.map_err(|e| format!("Error: {:?}", e).into_response())?
+        },
         Some(p) => p,
     };
 
@@ -134,6 +137,7 @@ async fn create_project(Path(project): Path<String>, State(database): State<Data
         return "Project name cannot contain a '/'".to_string();
     }
 
+    println!("Creating new project: {}", project);
     database.create_project(&project).await.unwrap();
 
     format!("{:?}", project)

database/src/project.rs
@@ -474,7 +474,7 @@ mod methods {
         let data = project.get_data().await.unwrap();
 
         assert_eq!(data.len(), 1);
-        assert_eq!(data[0].get("boo"), Some("bar".to_string()).as_ref());
+        assert_eq!(data[0].get("boo"), Some(Some("bar".to_string())).as_ref());
     }
 
     impl Project {